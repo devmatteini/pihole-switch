d7f2e7840a19c1a74b0c8adfca9905dec391ec80
devmatteini <dev.matteini@gmail.com>
2020-10-31 17:34:00 +0100

    tests: add system tests for command line tool

---
+++ tests/cli_tests.rs
@@ -0,0 +1,49 @@
+mod support;
+
+#[cfg(test)]
+mod cli_tests {
+    use std::process::Command;
+
+    use assert_cmd::assert::OutputAssertExt;
+    use assert_cmd::prelude::CommandCargoExt;
+
+    use support::pihole_server::PiHoleServer;
+
+    use super::*;
+
+    #[test]
+    fn successfully_enabled() {
+        let api_token = "VALID_TOKEN".to_string();
+        let url = PiHoleServer::new(api_token).start().unwrap();
+        let host = get_host(&url);
+
+        let mut cmd = Command::cargo_bin("phs").unwrap();
+
+        let assert = cmd
+            .env_clear()
+            .args(&["--host", &host])
+            .args(&["enable", "VALID_TOKEN"])
+            .assert();
+
+        assert
+            .success()
+            .stdout(predicates::str::contains("enabled successfully"));
+    }
+
+    #[test]
+    fn no_api_token_provided() {
+        let mut cmd = Command::cargo_bin("phs").unwrap();
+
+        let assert = cmd.env_clear().arg("enable").assert();
+
+        assert
+            .success()
+            .stderr(predicates::str::contains("No api token was provided"));
+    }
+
+    fn get_host(url: &str) -> String {
+        let parts: Vec<&str> = url.split("http://").collect();
+
+        parts[1].to_string()
+    }
+}
