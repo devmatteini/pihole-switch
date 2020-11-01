775b37b0acda29b44860a85ac7a04fe9d61d71fb
devmatteini <dev.matteini@gmail.com>
2020-11-01 17:58:33 +0100

    cli - io: use ansi_term to print colored output

--- src/cli/io.rs
+++ src/cli/io.rs
@@ -1,19 +1,37 @@
+use ansi_term::{Color, Style};
+
 use pihole_switch::pihole::PiHoleError;
 
 pub fn print_success(value: &str) {
-    println!("[✓] {}", value);
+    print_bold(Color::Green, &format!("[✓] {}", value));
 }
 
 pub fn print_error(value: &str) {
-    eprintln!("[X] {}", value);
+    eprint_bold(Color::Red, &format!("[X] {}", value));
+}
+
+pub fn print_warning(value: &str) {
+    eprint_bold(Color::Yellow, &format!("[!] {}", value));
+}
+
+pub fn print_unknown(value: &str) {
+    eprint_bold(Color::White, &format!("[?] {}", value));
+}
+
+fn print_bold(color: Color, message: &str) {
+    println!("{}", Style::new().bold().fg(color).paint(message));
+}
+
+fn eprint_bold(color: Color, message: &str) {
+    eprintln!("{}", Style::new().bold().fg(color).paint(message));
 }
 
 pub fn print_pihole_error(error: PiHoleError) {
     match error {
         PiHoleError::BadRequestOrTokenNotValid => print_error(&error.to_string()),
-        PiHoleError::HttpError => eprintln!("[?] {}", error),
-        PiHoleError::InvalidResponse => eprintln!("[?] {}", error),
-        PiHoleError::NotEnabled => eprintln!("[!] {}", error),
-        PiHoleError::NotDisabled => eprintln!("[!] {}", error),
+        PiHoleError::HttpError => print_unknown(&error.to_string()),
+        PiHoleError::InvalidResponse => print_unknown(&error.to_string()),
+        PiHoleError::NotEnabled => print_warning(&error.to_string()),
+        PiHoleError::NotDisabled => print_warning(&error.to_string()),
     }
 }
