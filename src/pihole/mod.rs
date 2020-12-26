fb611c55fce924802a7f2daceb6a1d0948ff6fc4
devmatteini <dev.matteini@gmail.com>
2020-12-26 18:25:17 +0100

    pihole: extracted PiholeResult type alias

--- src/pihole/mod.rs
+++ src/pihole/mod.rs
@@ -23,7 +23,9 @@ impl ExpectedStatus {
     }
 }
 
-pub fn enable(config: &PiHoleConfig) -> Result<(), PiHoleError> {
+type PiholeResult = Result<(), PiHoleError>;
+
+pub fn enable(config: &PiHoleConfig) -> PiholeResult {
     let url = format!("{}?enable&auth={}", &config.api_url, &config.api_token);
     let response = request(&url)?;
 
@@ -32,7 +34,7 @@ pub fn enable(config: &PiHoleConfig) -> Result<(), PiHoleError> {
     process_response(json, ExpectedStatus::Enabled, PiHoleError::NotEnabled)
 }
 
-pub fn disable(config: &PiHoleConfig, time: PiHoleDisableTime) -> Result<(), PiHoleError> {
+pub fn disable(config: &PiHoleConfig, time: PiHoleDisableTime) -> PiholeResult {
     let url = format!(
         "{}?disable={}&auth={}",
         &config.api_url,
@@ -66,7 +68,7 @@ fn process_response(
     json: JsonValue,
     expected_status: ExpectedStatus,
     status_error: PiHoleError,
-) -> Result<(), PiHoleError> {
+) -> PiholeResult {
     match json {
         JsonValue::Object(obj) => match obj.get(&"status".to_string()) {
             Some(actual) => validate_status(expected_status, actual, status_error),
@@ -81,7 +83,7 @@ fn validate_status(
     expected: ExpectedStatus,
     actual: &JsonValue,
     error: PiHoleError,
-) -> Result<(), PiHoleError> {
+) -> PiholeResult {
     if actual == &JsonValue::from(expected.as_str()) {
         Ok(())
     } else {
