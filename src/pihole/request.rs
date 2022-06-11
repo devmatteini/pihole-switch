f60e1a04e4c665be0e576ea0b65f80647da604fb
Cosimo Matteini <dev.matteini@gmail.com>
2022-06-11 02:44:38 +0200

    pihole: do not show url on error messages to avoid leaking the api token

--- src/pihole/request.rs
+++ src/pihole/request.rs
@@ -6,10 +6,21 @@ pub fn get(url: &str) -> Result<JsonValue, PiHoleError> {
     ureq::get(url)
         .timeout(std::time::Duration::from_secs(7))
         .call()
-        .map_err(|e| PiHoleError::HttpError(e.to_string()))
+        .map_err(pihole_error)
         .and_then(deserialize)
 }
 
 fn deserialize(x: ureq::Response) -> Result<JsonValue, PiHoleError> {
     x.into_json().map_err(|_| PiHoleError::InvalidResponse)
+}
+
+fn pihole_error(error: ureq::Error) -> PiHoleError {
+    match error {
+        ureq::Error::Transport(e) => {
+            let message = e.message().unwrap_or("Unknown transport error");
+            let kind = e.kind();
+            PiHoleError::HttpError(format!("{} - {}", kind, message))
+        }
+        other => PiHoleError::HttpError(other.to_string())
+    }
 }
